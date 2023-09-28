36e38a985af471ebe3f0915f0dc238cea02b04de
christianjensen2903 <cmj290300@gmail.com>
2023-09-28 10:17:16 +0200

    Revert "✨ Make the UNet only have two layers"
    This reverts commit 9f99f0d3abe81131e00124a83de802d4cc0d3bde.

--- unet.py
+++ unet.py
@@ -99,20 +99,28 @@ class ContextUnet(nn.Module):
         self.n_classes = n_classes
 
         self.init_conv = ResidualConvBlock(in_channels, n_feat, is_res=True)
-        self.down1 = UnetDown(n_feat, 2 * n_feat)
+
+        self.down1 = UnetDown(n_feat, n_feat)
+        self.down2 = UnetDown(n_feat, 2 * n_feat)
 
         self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
 
         self.timeembed1 = EmbedFC(1, 2 * n_feat)
+        self.timeembed2 = EmbedFC(1, 1 * n_feat)
         self.contextembed1 = EmbedFC(n_classes, 2 * n_feat)
+        self.contextembed2 = EmbedFC(n_classes, 1 * n_feat)
 
         self.up0 = nn.Sequential(
-            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, 7, 7),
+            # nn.ConvTranspose2d(6 * n_feat, 2 * n_feat, 7, 7), # when concat temb and cemb end up w 6*n_feat
+            nn.ConvTranspose2d(
+                2 * n_feat, 2 * n_feat, 7, 7
+            ),  # otherwise just have 2*n_feat
             nn.GroupNorm(8, 2 * n_feat),
             nn.ReLU(),
         )
 
         self.up1 = UnetUp(4 * n_feat, n_feat)
+        self.up2 = UnetUp(2 * n_feat, n_feat)
         self.out = nn.Sequential(
             nn.Conv2d(2 * n_feat, n_feat, 3, 1, 1),
             nn.GroupNorm(8, n_feat),
@@ -126,7 +134,8 @@ class ContextUnet(nn.Module):
 
         x = self.init_conv(x)
         down1 = self.down1(x)
-        hiddenvec = self.to_vec(down1)
+        down2 = self.down2(down1)
+        hiddenvec = self.to_vec(down2)
 
         # convert context to one hot embedding
         c = nn.functional.one_hot(c, num_classes=self.n_classes).type(torch.float)
@@ -140,8 +149,15 @@ class ContextUnet(nn.Module):
         # embed context, time step
         cemb1 = self.contextembed1(c).view(-1, self.n_feat * 2, 1, 1)
         temb1 = self.timeembed1(t).view(-1, self.n_feat * 2, 1, 1)
+        cemb2 = self.contextembed2(c).view(-1, self.n_feat, 1, 1)
+        temb2 = self.timeembed2(t).view(-1, self.n_feat, 1, 1)
+
+        # could concatenate the context embedding here instead of adaGN
+        # hiddenvec = torch.cat((hiddenvec, temb1, cemb1), 1)
 
         up1 = self.up0(hiddenvec)
-        up2 = self.up1(cemb1 * up1 + temb1, down1)
-        out = self.out(torch.cat((up2, x), 1))
+        # up2 = self.up1(up1, down2) # if want to avoid add and multiply embeddings
+        up2 = self.up1(cemb1 * up1 + temb1, down2)  # add and multiply embeddings
+        up3 = self.up2(cemb2 * up2 + temb2, down1)
+        out = self.out(torch.cat((up3, x), 1))
         return out
